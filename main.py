6fc1092633383ff30480fb4b67fc517dc0d822ec
sigmaunited543 <ivankalpun@gmail.com>
2026-05-31 15:23:34 +0500

    3 commit

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
-db = SQLAlchemy(app)
-
 from models import Task
 
 @app.route("/")
@@ -12,17 +8,3 @@ def index():
     tasks = Task.query.all()
     return render_template("index.html", tasks=tasks)
 
-@app.route("/add", methods=["POST"])
-def add_task():
-    content = request.form.get("content")
-    if content:
-        db.session.add(Task(content=content))
-        db.session.commit()
-    return redirect("/")
-
-@app.route("/delete/<int:task_id>")
-def delete_task(task_id):
-    task = Task.query.get(task_id)
-    db.session.delete(task)
-    db.session.commit()
-    return redirect("/")
