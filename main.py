0f8469c7b4c8f5974bfb622acc5248a047a6bdd6
sigmaunited543 <ivankalpun@gmail.com>
2026-06-07 15:51:19 +0500

    3 commit

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ def index():
     title = "Octask"
     return render_template("index.html")
 
-@app.route("/other")
-def other():
-    return render_template("other.html")
 
 
 if __name__ == "__main__":
